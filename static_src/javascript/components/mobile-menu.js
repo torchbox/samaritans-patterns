0fda4b6fe9e4516689f813a86e0468906db13802
Chris Lawton <chris.lawton@torchbox.com>
2022-03-07 10:30:47 +0000

    Update from samaritans.org 53f8e08e

--- static_src/javascript/components/mobile-menu.js
+++ static_src/javascript/components/mobile-menu.js
@@ -1,3 +1,5 @@
+import { createFocusTrap } from 'focus-trap';
+
 class MobileMenu {
     static selector() {
         return '.js-mobile-menu-toggle';
@@ -7,40 +9,73 @@ class MobileMenu {
         this.node = node;
         this.body = document.getElementsByTagName('body')[0];
         this.headerNav = document.querySelector('.js-header-nav');
-        this.animatedClass = 'js-header-nav--is-animated';
         this.menuActiveClass = 'mob-nav-open';
-
-        this.state = {
-            open: false,
-        };
+        this.animateClass = 'animate-mob-nav';
+        this.visibleClass = 'is-visible';
+        this.openButton = document.querySelector('.js-open-mobile-menu');
+        this.closeButton = document.querySelector('.js-close-mobile-menu');
+        this.fistMenuItem = document.querySelector('.js-open-header-menu');
+        this.focusTrap = createFocusTrap(this.headerNav);
 
         this.bindEventListeners();
     }
 
     bindEventListeners() {
-        this.node.addEventListener('click', () => {
-            this.toggle();
-        });
+        this.openButton.addEventListener('click', () => this.handleOpen());
+        this.closeButton.addEventListener('click', () => this.handleClose());
 
-        this.updatedAnimatedClass();
+        document.addEventListener('keydown', (e) => {
+            if (e.key === 'Escape') {
+                this.handleClose();
+            }
+        });
     }
 
-    toggle() {
-        this.body.classList.toggle(this.menuActiveClass);
+    handleOpen() {
+        this.node.setAttribute('aria-expanded', 'true');
+
+        // remove 'display: none' from the menu
+        this.headerNav.classList.add(this.visibleClass);
+
+        setTimeout(() => {
+            // animate the menu in
+            this.body.classList.add(this.animateClass);
+        }, 100);
+
+        setTimeout(() => {
+            // hide the body content once the transition has finished
+            this.body.classList.add(this.menuActiveClass);
+
+            // move focus to the first menu item
+            this.fistMenuItem.focus();
+        }, 260);
+
+
+        // activate focus trap
+        this.focusTrap.activate();
     }
 
-    updatedAnimatedClass() {
-        let timeout;
+    handleClose() {
+        this.node.setAttribute('aria-expanded', 'false');
 
-        window.addEventListener('resize', () => {
-            clearTimeout(timeout);
+        // show the body content
+        this.body.classList.remove(this.menuActiveClass);
 
-            this.headerNav.classList.remove(this.animatedClass);
+        setTimeout(() => {
+            // animate the menu out
+            this.body.classList.remove(this.animateClass);
+        }, 100);
 
-            timeout = setTimeout( () => {
-                this.headerNav.classList.add(this.animatedClass);
-            }, 100);
-        });
+        setTimeout(() => {
+            // add 'display: none' to the menu so it can't be focussed
+            this.headerNav.classList.remove(this.visibleClass);
+
+            // move the focus to the first nav item
+            document.querySelector('.nav__link').focus();
+        }, 260);
+
+        // remove focus trap
+        this.focusTrap.deactivate();
     }
 }
 
