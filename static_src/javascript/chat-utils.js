7e5847671e9394576a9fec1a5794ccdc32258d39
Tomasz Knapik <tomasz.knapik@torchbox.com>
2022-03-29 10:52:43 +0000

    Update from samaritans.org e4706fc1

--- static_src/javascript/chat-utils.js
+++ static_src/javascript/chat-utils.js
@@ -10,15 +10,30 @@ document.addEventListener('DOMContentLoaded', function () {
         el.style.display = '';
     });
 
-
-    var api_url = JSON.parse(
+    const rawApiUrl = JSON.parse(
         document.getElementById('webchat_api_url').textContent
     );
+    const apiUrl = rawApiUrl ? new URL(rawApiUrl) : null;
 
     const chatCTA = document.querySelector(
         '[data-cta-block-large][data-cta-type="chat"]'
     );
+    const smallChatCTA = document.querySelector(
+        '[data-cta-block-small][data-cta-type="chat"]'
+    );
 
+    // There can be several chat CTAs on the same page - primary, sticky, and tabccordion
+    const otherCtas = document.querySelectorAll(
+        '[data-listening-cta-type="chat"], [data-tabccordion-cta-type="chat"], [data-service-cta-type="chat"]'
+    );
+    if (apiUrl) {
+        // Find the first CTA that has a skill set (if any have it) and assume
+        // that all other CTAs on the page are related to this webchat skill.
+        const ctaSkill = [chatCTA, smallChatCTA, ...otherCtas].filter(Boolean).map(element => element.dataset.ctaSkill).find(Boolean);
+        if (ctaSkill) {
+            apiUrl.searchParams.set('skill', ctaSkill);
+        }
+    }
 
     // small and large CTA are rendered as a pair in joint 3rd place.
     // they are first and last of type respectively
@@ -31,9 +46,6 @@ document.addEventListener('DOMContentLoaded', function () {
         // * No large chat CTA is present
         // * Chat CTA exists as a small CTA
 
-        const smallChatCTA = document.querySelector(
-            '[data-cta-block-small][data-cta-type="chat"]'
-        );
         if (smallChatCTA || !chatCTA) return;
 
         // promote the 3rd place small cta to a large one
@@ -41,10 +53,6 @@ document.addEventListener('DOMContentLoaded', function () {
         largeCTA && showListeningCTA(largeCTA);
     };
 
-    // There can be several chat CTAs on the same page - primary, sticky, and tabccordion
-    var otherCtas = document.querySelectorAll(
-        '[data-listening-cta-type="chat"], [data-tabccordion-cta-type="chat"], [data-service-cta-type="chat"]'
-    );
     const getChatUnavailableCtas = () => document.querySelectorAll('[data-cta-type="chat_unavailable"]')
     const showChatUnavailableCtas = () => {
         getChatUnavailableCtas().forEach(el => {
@@ -69,7 +77,7 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     };
 
-    if (!api_url) {
+    if (!apiUrl) {
         hideCustomChatElements();
         // Bail if we don't have an API URL. Make sure the element is removed
         hideListeningCTA(chatCTA);
@@ -139,7 +147,8 @@ document.addEventListener('DOMContentLoaded', function () {
             }
         }
     };
-    xhr.open('GET', api_url);
+    xhr.open('GET', apiUrl.href);
     xhr.timeout = 2000;
     xhr.send();
+
 });
--- webpack.config.js
+++ webpack.config.js
@@ -3,17 +3,21 @@ const webpack = require('webpack');
 
 const options = {
     entry: {
-        website: './static_src/javascript/main-website.js',
-        donate: './static_src/javascript/donate-website.js',
-        payments: './static_src/javascript/payments-website.js',
-        payments_monthly: './static_src/javascript/payments-monthly-website.js',
-        loqate: './static_src/javascript/loqate.js',
-        event: './static_src/javascript/event-website.js',
-        'chat-utils': './static_src/javascript/chat-utils.js'
+        'website': './static_src/javascript/main-website.js',
+        'donate': './static_src/javascript/donate-website.js',
+        'payments': './static_src/javascript/payments-website.js',
+        'payments_monthly':
+            './static_src/javascript/payments-monthly-website.js',
+        'loqate': './static_src/javascript/loqate.js',
+        'event': './static_src/javascript/event-website.js',
+        'chat-utils': [
+            'url-polyfill',
+            './static_src/javascript/chat-utils.js',
+        ],
     },
     output: {
         path: path.resolve('./static_compiled/js/'),
-        filename: '[name].js' // based on entry name, e.g. main.js
+        filename: '[name].js', // based on entry name, e.g. main.js
     },
     plugins: [
         new webpack.IgnorePlugin(/^\.\/locale$/, /moment$/, /moment-timezone$/),
@@ -25,12 +29,12 @@ const options = {
                 test: /\.js$/,
                 loader: 'babel-loader',
                 exclude: /node_modules/,
-            }
-        ]
+            },
+        ],
     },
     // externals are loaded via base.html and not included in the webpack bundle.
     externals: {
-    //gettext: 'gettext',
+        //gettext: 'gettext',
     },
 };
 
@@ -38,7 +42,6 @@ const options = {
   If a project requires internationalisation, then include `gettext` in base.html
     via the Django JSi18n helper, and uncomment it from the 'externals' object above.
 */
-
 
 if (process.env.NODE_ENV === 'development') {
     // Create JS source maps in the dev mode
